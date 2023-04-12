e66f7c960b565012c78f60bb0e1692c541b0eba7
Kazuki_Akimoto <116004457+Akimoto-IT0412@users.noreply.github.com>
2023-04-12 22:26:25 +0900

    sideBar.tsx修正

--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -1,38 +1,43 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
 
-export const SideBer = () => {
+export const SideBar = () => {
 
     const [open, setOpen] = useState<boolean>(true);
     const router = useRouter();
 
+    const Menus = [
+        {title:"Home", url:"/", svg:"M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"},
+        {title:"User", url:"/User", svg:"M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"},
+    ];
+
     return(
         <>
-        <nav
-            className={`${open ? "w-60":"w-20"} p-5 pt-8 duration-300 relative`}
-            >
+            <div className={`${open ? "w-60" : "w-20"} p-5 pt-8 duration-300`}>
                 <div className="relative">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`p-2 absolute cursor-pointer -right-9 rounded-full top-9 w-9 h-9 bg-white border-2 border-indigo-500 hover:border-indigo-800 ${!open && "rotate-180"}`} onClick={() => setOpen(!open)}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
                     </svg>
                 </div>
-                <div className="flex gap-x-4 items-center relative">
+                <div className="gap-x-4 items-center relative">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="2 0 24 24" strokeWidth={1.5} stroke="currentColor" className="absolute left-2 cursor-pointer w-7 h-7 duration-500" onClick={() => router.push("/")}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
                     </svg>
                     <h1 className={`ml-14 text-black origin-left font-medium text-xl duration-200  ${!open && "scale-0"}`}>Kadai</h1>
                 </div>
-                <ul className="pt-6">
-                    <li className="absolute text-gray-600 font-medium text-md flex gap-x-6 cursor-pointer items-center p-2 hover:text-indigo-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                        </svg>
-                        <span className={`${!open && "hidden"} origin-left duration-200`}>Home</span>
-                    </li>
+                <ul className="pt-6 fixed">
+                    {Menus.map((menu, i) => (
+                        <li className={`${router.pathname === `${menu.url}` ? "text-indigo-600" : "text-gray-600"} flex rounded-md p-2 my-4 cursor-pointer text-md items-center gap-x-4 hover:text-indigo-600`} key={i}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ">
+                                <path strokeLinecap="round" strokeLinejoin="round" d={`${menu.svg}`} />
+                            </svg>
+                            <span className={`absolut ${!open && "hidden"} origin-left duration-200`}>{menu.title}</span>
+                        </li>
+                    ))}
                 </ul>
-        </nav>
+            </div>
         </>
     )
 }
 
-export default SideBer;
+export default SideBar;
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,10 +1,10 @@
-import SideBer from "@/components/sideBer";
+import SideBar from "@/components/sideBar";
 
 export const Home = () => {
     return (
         <>
             <div className="flex">
-                <SideBer/>
+                <SideBar/>
             <article className="h-screen flex-1 bg-indigo-50">
                 <h1 className="text-borld text-xl p-5">Home</h1>
                 <div>
@@ -30,8 +30,8 @@ export const Home = () => {
                                 </select>
                         </div>
                         <div className="m-5 p-2 bg-white border border-gray-300 rounded-xl hover:border-indigo-500">
-                            <div className="flex">
-                                <div className="group relative">
+                            <div className="z-20 flex">
+                                <div className="group relative p-4">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22" fill="currentColor" className="w-16 hover:scale-110">
                                         <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clipRule="evenodd" />
                                     </svg>
@@ -39,13 +39,14 @@ export const Home = () => {
                                 <div className="mr-auto mx-4">
                                     <h3 className="text-xl font-bold text-gray-800">タイトル</h3>
                                     <p>科目名</p>
+                                    <p>view</p>
                                     <p>2023/4/12</p>
                                 </div>
-                                <div className="ml-auto mx-4">
-                                    <button>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hover:stroke-2">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
-                                    </svg>
+                                <div className="z-0 top-0 right-0 relative">
+                                    <button onClick={() => alert("ブックマークに追加")}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hover:stroke-2">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+                                        </svg>
                                     </button>
                                 </div>
                             </div>
