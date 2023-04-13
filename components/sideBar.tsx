9208f37bfa28d3f5058f7a436c1aedea4a531505
Kazuki_Akimoto <116004457+Akimoto-IT0412@users.noreply.github.com>
2023-04-13 17:13:21 +0900

    コンテンツを複数表示するように修正

--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -13,7 +13,7 @@ export const SideBar = () => {
 
     return(
         <>
-            <div className={`${open ? "w-60" : "w-20"} p-5 pt-8 duration-300`}>
+            <div className={` ${open ? "w-60" : "w-20"} p-5 pt-8 duration-300`}>
                 <div className="relative">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`p-2 absolute cursor-pointer -right-9 rounded-full top-9 w-9 h-9 bg-white border-2 border-indigo-500 hover:border-indigo-800 ${!open && "rotate-180"}`} onClick={() => setOpen(!open)}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,12 +1,16 @@
 import SideBar from "@/components/sideBar";
+import contentList from "../../contents.json";
 
 export const Home = () => {
+
+    const examList = contentList.exams;
+
     return (
         <>
             <div className="flex">
                 <SideBar/>
             <article className="h-screen flex-1 bg-indigo-50">
-                <h1 className="text-borld text-xl p-5">Home</h1>
+                <h1 className="text-borld text-xl p-2">Home</h1>
                 <div>
                     <form>
                         <div className="relative m-5">
@@ -29,27 +33,31 @@ export const Home = () => {
                                     <option>昇順</option>
                                 </select>
                         </div>
-                        <div className="m-5 p-2 bg-white border border-gray-300 rounded-xl hover:border-indigo-500">
-                            <div className="z-20 flex">
-                                <div className="group relative p-4">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22" fill="currentColor" className="w-16 hover:scale-110">
-                                        <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clipRule="evenodd" />
-                                    </svg>
+                        <div className="my-4 overflow-y-scroll h-96">
+                            {examList.map((exam, i) =>
+                                <div className="m-5 p-2 bg-white border border-gray-300 rounded-xl hover:border-indigo-500" key={i}>
+                                    <div className="z-20 flex">
+                                        <div className="group relative p-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22" fill="currentColor" className="w-16 hover:scale-110">
+                                                <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clipRule="evenodd" />
+                                            </svg>
+                                        </div>
+                                        <div className="mr-auto mx-4">
+                                            <h3 className="text-xl font-bold text-gray-800">{exam.title}</h3>
+                                            <p>科目名:{exam.subjevtName}</p>
+                                            <p>view</p>
+                                            <p>{exam.postedAt}</p>
+                                        </div>
+                                        <div className="z-0 top-0 right-0 relative">
+                                            <button onClick={() => alert("ブックマークに追加")}>
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hover:stroke-2">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+                                                </svg>
+                                            </button>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="mr-auto mx-4">
-                                    <h3 className="text-xl font-bold text-gray-800">タイトル</h3>
-                                    <p>科目名</p>
-                                    <p>view</p>
-                                    <p>2023/4/12</p>
-                                </div>
-                                <div className="z-0 top-0 right-0 relative">
-                                    <button onClick={() => alert("ブックマークに追加")}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hover:stroke-2">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
+                            )}
                         </div>
                     </main>
                 </div>
