e8eeb9e3f996da8527d96a4a50f7034c276c2e60
kukizon0 <kukizonok@outlook.jp>
2023-05-01 12:23:49 +0900

    add CSSを必要最低限整えた

--- pages/detail/detail.tsx
+++ pages/detail/detail.tsx
@@ -1,39 +1,53 @@
 import Image from 'next/image'
-
+// import styles from '@/styles/Home.module.css'
+import { Stint_Ultra_Condensed } from 'next/font/google'
 export default function Detail(props:any) {
 
+  const ptag = "font-normal text-gray-700 dark:text-gray-400 pb-7"
+  const h3tag = "font-bold h3tag "
     return (
-        <div>
-             <Image
-            // className={styles.logo}
-            src={props.book.book_img_link}
-            alt="Next.js Logo"
-            width={150}
-            height={150}
-            priority
-          />
-          <div >
-            <h2>{props.book.book_title}</h2>
-            <p>{props.book.author}</p>
+      // <style jsx>{`
+      //   p {
+      //       display: grid;
+      //       place-items: center;
+      //   },
+      //   `}
+      // </style> 
+        <div className='container mt-20 ml-10'>
+          <div className='text-center'>
+            <Image
+              className='flex  mx-auto mt-10'
+              src={props.book.book_img_link}
+              alt="Next.js Logo"
+              width={150}
+              height={150}
+              // priority
+            />
+          
+              <h1 className='font-bold text-blue-800 text-4xl pt-6 pb-2'>{props.book.book_title}</h1>
+              <p className='font-normal text-gray-800 pb-8'>{props.book.book_author}</p>
+           
           </div>
+             
 
           <div>
-            <h3>{props.book.headline1}</h3>
-            <p>{props.book.content1}</p>
+            <h3 className={h3tag}>{props.book.headline1}</h3>
+            <p className={ptag}>{props.book.content1}</p>
           </div>
           
           <div>
-          <h3>{props.book.headline2}</h3>
-            <p>{props.book.content2}</p>
+          <h3 className={h3tag}>{props.book.headline2}</h3>
+            <p className={ptag}>{props.book.content2}</p>
           </div>
 
 
           <div>
-            <h3>{props.book.detail_explanation_title}</h3>
-            <p>{props.detail_explanation_content}</p>
+            <h3 className={h3tag}>{props.book.detail_explanation_title}</h3>
+            <p className={ptag}>{props.book.detail_explanation_content}</p>
           </div>
-
+        
         </div>
+        
     )
 }
 
