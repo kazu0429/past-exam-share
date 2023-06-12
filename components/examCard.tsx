caa3a6926a1ff63b22fdfb8640537692091ee454
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-06-12 11:22:23 +0900

    remove bookmarkリストの変更コード

--- components/examCard.tsx
+++ components/examCard.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { Exam } from "@/types/exam";
-import { useState, ReactElement, ReactNode, useEffect, useRef } from "react";
+import { useState, ReactElement,useEffect, useRef } from "react";
 import { useAuth } from "@/context/AuthContext";
 import { db } from "@/firebase/firebase";
 import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
@@ -9,55 +9,46 @@ import { User } from "@/types/user";
 type Props = {
     exam:Exam;
     icon:ReactElement | null;
+    canRemove:boolean;
 }
 
 export const ExamCard = (props:Props) => {
 
-    const { exam, icon } = props;
+    const { exam, icon ,canRemove } = props;
     const eid = exam.id as string;
     const user = useAuth() as User;
     const ref = useRef(true);
     const [ bookmark, setBookmark ] = useState<boolean>(false);
-    const [ bookmarkList, setBookmarkList ] = useState<Array<string>>([]);
 
     useEffect(() => {
-        if(ref.current){
+        if(ref.current || user === null){
             ref.current = false;
             return;
         }
-        console.log("bookmarks : ", user.bookmarks);
         if(user.bookmarks.includes(eid)){
             setBookmark(true);
-            bookmarkList.push(eid);
-            setBookmarkList(bookmarkList);
         }
     },[])
 
-    const updateBookmarkOfUser = async(eid:string) => {
+    const updateBookmarkOfUser = async() => {
         const userRef = doc(db, "users", user.id);
         try{
-            if(bookmarkList.includes(eid)){
-                const index = bookmarkList.indexOf(eid);
+            if(bookmark){
                 await updateDoc(userRef, {
                     bookmarks: arrayRemove(eid)
                 })
                 setBookmark(false);
-                bookmarkList.splice(index, 1);
-                setBookmarkList(bookmarkList)
                 // alert("ブックマークから削除しました。");
             }else{
                 await updateDoc(userRef, {
                     bookmarks: arrayUnion(eid)
                 })
                 setBookmark(true);
-                bookmarkList.push(eid);
-                setBookmarkList(bookmarkList);
                 alert("ブックマークに追加しました。");
             }
         }catch(err){
             console.log(err);
         }
-        console.log()
     }
 
     return (
@@ -73,16 +64,12 @@ export const ExamCard = (props:Props) => {
                     <p>投稿日 : {(new Date(exam.postedAt)).toLocaleString()}</p>
                 </div>
                 <div className="flex flex-col gap-y-3 z-0 top-0 right-0 relative">
-                    <button onClick={() => updateBookmarkOfUser(exam.id as string)}>
+                    {!canRemove && 
+                    (<button onClick={updateBookmarkOfUser}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6 hover:stroke-2 ${bookmark && "fill-indigo-500"}`}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                         </svg>
-                    </button>
-                    {/* <button onClick={() => alert("いいね")}>
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                                                </svg>
-                                            </button> */}
+                    </button>)}
                 </div>
             </div>
         </div>
