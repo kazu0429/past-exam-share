9d39d64d2fbf6253baa3bce5f66345b657434669
kazu0429 <116004457+Akimoto-IT0412@users.noreply.github.com>
2023-06-28 01:51:33 +0900

    change bookmarkへの登録を削除し、投稿自体のbookmark登録数表示

--- components/examCard.tsx
+++ components/examCard.tsx
@@ -5,32 +5,47 @@ import { useAuth } from "@/context/AuthContext";
 import { db } from "@/firebase/firebase";
 import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
 import { User } from "@/types/user";
+import BookMark from "@/pages/bookmark";
+
+
+
 
 type Props = {
     exam:Exam;
     icon:ReactElement | null;
     canRemove:boolean;
-    bookmarks:string[]|null;
+    isbookmark:boolean;
 }
 
 export const ExamCard = (props:Props) => {
 
-    const { exam, icon ,canRemove, bookmarks } = props;
-    const eid = exam?.id as string;
+    const { exam, icon ,canRemove, isbookmark } = props;
+    // let bookmark = isbookmark
+    const eid = exam.id as string;
     const user = useAuth() as User;
     const ref = useRef(true);
-    const [ bookmark, setBookmark ] = useState<boolean>(false);
+
+    const [bookmark, setBookmark] = useState<boolean>(props.isbookmark);
 
     useEffect(() => {
         if(ref.current){
             ref.current = false;
             return;
         }
-        console.log(bookmarks)
-        if(bookmarks?.includes(eid)){
-            setBookmark(true);
-        }
     },[])
+
+    const bookmarkCountView = (num: number) => {
+        if (num < 1000) {
+            return num.toString();
+        } else if (num < 1000000) {
+            const thousandsCount = Math.floor(num / 1000);
+            return thousandsCount.toString() + "k";
+        } else {
+            const millionsCount = Math.floor(num / 1000000);
+            return millionsCount.toString() + "m";
+        }
+    }
+
 
     const updateBookmarkOfUser = async() => {
         const userRef = doc(db, "users", user.id);
@@ -39,15 +54,14 @@ export const ExamCard = (props:Props) => {
                 await updateDoc(userRef, {
                     bookmarks: arrayRemove(eid)
                 })
-                setBookmark(false);
                 // alert("ブックマークから削除しました。");
             }else{
                 await updateDoc(userRef, {
                     bookmarks: arrayUnion(eid)
                 })
-                setBookmark(true);
                 alert("ブックマークに追加しました。");
             }
+            setBookmark(!bookmark)
         }catch(err){
             console.log(err);
         }
@@ -66,11 +80,14 @@ export const ExamCard = (props:Props) => {
                 </div>
                 <div className="flex flex-col gap-y-3 z-0 top-0 right-0 relative">
                     {!canRemove && 
-                    (<button onClick={updateBookmarkOfUser}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6 hover:stroke-2 ${bookmark && "fill-indigo-500"}`}>
+                    (<div className="flex flex-col items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6`}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                         </svg>
-                    </button>)}
+                        <div className="text-sm">
+                            {bookmarkCountView(exam.booknum)}
+                        </div>
+                    </div>)}
                 </div>
             </div>
         </div>
