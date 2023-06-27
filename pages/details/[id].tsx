c9ed634856bc970c2d2295b6667847e08b813ed6
kazu0429 <116004457+Akimoto-IT0412@users.noreply.github.com>
2023-06-28 01:53:20 +0900

    add 投稿の詳細画面表示

--- pages/details/[id].tsx
+++ pages/details/[id].tsx
@@ -1,13 +1,17 @@
 import { ReactElement, useEffect, useState } from "react";
 import SideBar from "@/components/sideBar";
-import { db } from "@/firebase/firebase";
-import { doc, updateDoc, arrayRemove, arrayUnion } from "firebase/firestore";
+import { db, storage } from "@/firebase/firebase";
+import { doc, updateDoc, arrayRemove, arrayUnion, getDoc } from "firebase/firestore";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { GetFileData, GetPostData, GetPostUserIcon } from "../../utils/getPostData";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import '@splidejs/splide/css'; 
 import { useAuth } from "@/context/AuthContext";
 import { User } from "@/types/user";
+import dynamic from "next/dynamic";
+import { MouseEvent } from "react";
+import { getDownloadURL, listAll, ref } from "firebase/storage";
+
 
 
 export const Details = ({postData}:InferGetServerSidePropsType<typeof getServerSideProps>) =>{
@@ -22,29 +26,47 @@ export const Details = ({postData}:InferGetServerSidePropsType<typeof getServerS
         postedAt,
         pdf,
         images,
+        booknum,
     } = postData.data;
 
     const icon = postData.icon;
     const urls = postData.urls;
     const user = useAuth() as User;
-    
 
+    const PDFViewer = dynamic(() => import("../../components/pdfViewer"), {
+        ssr: false
+    });
+    
     const [ bookmark, setBookmark ] = useState<boolean>(false);
 
     useEffect(() => {
-        if(user){
-            if(user.bookmarks.includes(id)){
-                setBookmark(true);
+        (async() => {
+            try{
+                if(user){
+                    const bookRef = doc(db, "users", user.id);
+                    const snapshot = await getDoc(bookRef);
+                    const bookmarks = snapshot.get("bookmarks");
+                    if(bookmarks.includes(id)){
+                        setBookmark(true);
+                    }
+                }
+            }catch(err){
+                console.log(err);
             }
-        }
+        })()
     },[])
+
 
     const updateBookmarkOfUser = async() => {
         const userRef = doc(db, "users", user.id);
+        const postRef = doc(db, "exams", id);
         try{
             if(bookmark){
                 await updateDoc(userRef, {
                     bookmarks: arrayRemove(id)
+                })
+                await updateDoc(postRef,{
+                    booknum:booknum-1
                 })
                 setBookmark(false);
                 alert("ブックマークから削除しました。");
@@ -52,12 +74,44 @@ export const Details = ({postData}:InferGetServerSidePropsType<typeof getServerS
                 await updateDoc(userRef, {
                     bookmarks: arrayUnion(id)
                 })
+                await updateDoc(postRef,{
+                    booknum:booknum+1
+                })
                 setBookmark(true);
                 alert("ブックマークに追加しました。");
             }
         }catch(err){
             console.log(err);
         }
+    }
+
+    const downloadFile = async(e:MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        try{
+            const downloadRef = ref(storage, `contens/${id}`);
+            const documentList = await listAll(downloadRef);
+            documentList.items.forEach((fileRef) => {
+                getDownloadURL(fileRef).then((url) => {
+                    const xhr = new XMLHttpRequest();
+                    xhr.responseType = 'blob';
+                    xhr.onload = (event) => {
+                        const blob = xhr.response;
+                        const blobUrl = URL.createObjectURL(blob)
+                        const link = document.createElement('a')
+                        link.href = blobUrl
+                        link.download = fileRef.name
+                        link.click()
+                        console.log(blob)
+                    };
+                    xhr.open('GET', url);
+                    xhr.send();
+                })
+            })
+        }catch(err){
+            console.log(err)
+        }
+        
+
     }
 
     return (
@@ -79,9 +133,9 @@ export const Details = ({postData}:InferGetServerSidePropsType<typeof getServerS
                                 {title}
                             </div>
                                 <div className="m-auto relative group ">
-                                    <span className="tooltip right-0 before:left-3/4">投稿を保存</span>
+                                    <span className="tooltip whitespace-nowrap right-0 before:left-3/4">投稿を保存</span>
                                     <button onClick={updateBookmarkOfUser}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6 hover:stroke-2 ${bookmark && "fill-indigo-500"}`}>
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
                                         </svg>
                                     </button>
@@ -102,14 +156,19 @@ export const Details = ({postData}:InferGetServerSidePropsType<typeof getServerS
                                 <div>
                                     <div className="relative group">
                                         <span className="tooltip">資料を保存</span>
-                                        <button className="button">
+                                        <button className="button" onClick={(e) => downloadFile(e)}>
                                             ダウンロード
                                         </button>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="py-3 border border-indigo-800 rounded-xl bg-indigo-100">
+                        { fileType === ".pdf" ? (
+                            <div>
+                                <PDFViewer fileType={fileType} pdf={pdf} id={id}/>
+                            </div>
+                        ):(
+                            <div className="py-3 border border-indigo-800 rounded-xl bg-indigo-100">
                             <Splide
                                 aria-label="投稿画像一覧"
                                 options={{
@@ -135,6 +194,8 @@ export const Details = ({postData}:InferGetServerSidePropsType<typeof getServerS
                                 ))}
                             </Splide>
                         </div>
+                        )
+                        }
                     </div>
                 </div>
             </article>
